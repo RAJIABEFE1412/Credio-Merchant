d626fc1e3983beb23d027d765b32be3339f96d42
Precious1324 <aleriwaprecious70@gmail.com>
2022-12-08 22:59:10 -0800

    deploy react app

--- src/Components/Input/PinField.js
+++ src/Components/Input/PinField.js
@@ -1,10 +1,17 @@
 import './PinField.css';
+import { useCallback } from 'react';
 const PinField = () => {
+    const emailInput = useCallback((inputElement) => {
+        if (inputElement) {
+          inputElement.focus();
+        }
+      }, []);
     return ( 
         <div className="pinfield">
             <input
                 type="text"
                 maxlength= "1"
+                ref={emailInput}
             ></input>
         </div>
     );
--- src/Pages/Withdraw/CardPin.js
+++ src/Pages/Withdraw/CardPin.js
@@ -1,11 +1,48 @@
 import { Link } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
 import { BsArrowLeft } from "react-icons/bs";
+import { useCallback } from 'react';
 import './CardPin.css'
 import PinField from "../../Components/Input/PinField";
 import { useState } from "react";
 import ModalSaving from "../../Components/Modal/ModalSaving";
 const CardPin = () => {
     const [openModal, setOpenModal] = useState(false);
+    const [pin, setPin] = useState("");
+    const atmpin = useRef(null);
+    useEffect(()=>{
+    if(pin.length === 1){
+        atmpin1.current.focus();
+    }
+    }, [pin.length]);
+    const onChangepin1 = (e) => {
+        setPin(e.target.value)
+    }
+    const [pin1, setPin1] = useState("");
+    const atmpin1 = useRef(null);
+    useEffect(()=>{
+        if(pin1.length === 1){
+            atmpin2.current.focus();
+        }
+    }, [pin1.length]);
+    const onChangepin2 = (e) => {
+        setPin1(e.target.value)
+    }
+    const [pin2, setPin2] = useState("");
+    const atmpin2 = useRef(null);
+    useEffect(()=>{
+        if(pin2.length === 1){
+            atmpin3.current.focus();
+        }
+    }, [pin2.length]);
+    const onChangepin3 = (e) => {
+        setPin2(e.target.value)
+    }
+    const [pin3, setPin3] = useState("");
+    const atmpin3 = useRef(null);
+    const onChangepin4 = (e) => {
+        setPin3(e.target.value)
+    }
     return ( 
         <div className="cardpin">
             <div className="deposit-title">
@@ -21,16 +58,49 @@ const CardPin = () => {
                     <p className="enter-pin">Please Enter Your Card Pin</p>
                     <div className="field-container">
                         <div className="field-1">
-                            <PinField />
+                            <div className="pinfield">
+                                <input
+                                    type="text"
+                                    maxlength= "1"
+                                    value={pin}
+                                    onChange={onChangepin1}
+                                    ref={atmpin}
+                                    autoFocus
+                                ></input>
+                            </div>
                         </div>
                         <div className="field-1">
-                            <PinField/>
+                            <div className="pinfield">
+                                <input
+                                    type="text"
+                                    maxlength= "1"
+                                    value={pin1}
+                                    onChange={onChangepin2}
+                                    ref={atmpin1}
+                                ></input>
+                            </div>
                         </div>
                         <div className="field-1">
-                            <PinField />
+                            <div className="pinfield">
+                                <input
+                                    type="text"
+                                    maxlength= "1"
+                                    value={pin2}
+                                    onChange={onChangepin3}
+                                    ref={atmpin2}
+                                ></input>
+                            </div>
                         </div>
                         <div className="field-1">
-                            <PinField />
+                            <div className="pinfield">
+                                <input
+                                    type="text"
+                                    maxlength= "1"
+                                    value={pin3}
+                                    onChange={onChangepin4}
+                                    ref={atmpin3}
+                                ></input>
+                            </div>
                         </div>
                     </div>
                     <div className="form-submit-right saving-submit withdraw-submit">
