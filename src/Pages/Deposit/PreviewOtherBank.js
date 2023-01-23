2559894bd5b48a34bd7ecaa8905c3a803433f0aa
Precious1324 <aleriwaprecious70@gmail.com>
2023-01-22 21:13:15 -0800

    editted code

--- src/Pages/Deposit/PreviewOtherBank.js
+++ src/Pages/Deposit/PreviewOtherBank.js
@@ -3,18 +3,25 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { useState, useEffect, useRef } from "react";
 import "./PreviewOtherBank.css";
+import JSEncrypt from 'jsencrypt';
+import consts from '../Login/keys/const'
 import Modal from "../../Components/Modal/Modal";
 import Navbar from "../../Components/Navbar/Navbar";
 import { fetchBank, postData, reqData } from "../../Redux/Bank/BankAction";
+import { depositData } from "../../Redux/Deposit/DepositAction";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 const PreviewOtherBank = ({
   bankData,
   fetchBank,
   postData,
   nameData,
+  depositData,
   reqData,
+  deposit
 }) => {
   const [openModal, setOpenModal] = useState(false);
+  const [depositstate, setDepositState] = useState({})
+  const [combinedpin, setCombinedpin] = useState('');
   const [pin, setPin] = useState("");
   const atmpin = useRef(null);
   useEffect(() => {
@@ -23,7 +30,11 @@ const PreviewOtherBank = ({
     }
   }, [pin.length]);
   const onChangepin1 = (e) => {
-    setPin(e.target.value);
+    const value = e.target.value
+    // var encrypt = new JSEncrypt();
+    // encrypt.setPublicKey(`${consts.pub_key}`);
+    // var encrypted = encrypt.encrypt(value);
+    setPin(value)
   };
   const [pin1, setPin1] = useState("");
   const atmpin1 = useRef(null);
@@ -33,7 +44,11 @@ const PreviewOtherBank = ({
     }
   }, [pin1.length]);
   const onChangepin2 = (e) => {
-    setPin1(e.target.value);
+    const value = e.target.value
+    // var encrypt = new JSEncrypt();
+    // encrypt.setPublicKey(`${consts.pub_key}`);
+    // var encrypted = encrypt.encrypt(value);
+    setPin1(value)
   };
   const [pin2, setPin2] = useState("");
   const atmpin2 = useRef(null);
@@ -43,20 +58,59 @@ const PreviewOtherBank = ({
     }
   }, [pin2.length]);
   const onChangepin3 = (e) => {
-    setPin2(e.target.value);
-  };
-  const getName = () => {
-    console.log("nameData.bankname.transferData.beneficiaryBankCode", bankData.bank);
-    return bankData.bank.find(
-      (element) =>
-        element.routingKey == nameData.bankname.transferData.beneficiaryBankCode
-    ).name;
+    const value = e.target.value
+    // var encrypt = new JSEncrypt();
+    // encrypt.setPublicKey(`${consts.pub_key}`);
+    // var encrypted = encrypt.encrypt(value);
+    setPin2(value)
   };
   const [pin3, setPin3] = useState("");
   const atmpin3 = useRef(null);
   const onChangepin4 = (e) => {
-    setPin3(e.target.value);
+    const value = e.target.value
+    setPin3(value)
+    const pins = `${pin}${pin1}${pin2}${value}`
+    console.log(pins)
+    var encrypt = new JSEncrypt();
+    encrypt.setPublicKey(`${consts.pub_key}`);
+    var encrypted = encrypt.encrypt(pins);
+    console.log(encrypted)
+    setCombinedpin(encrypted);
   };
+
+ 
+  // const reference =Math.floor(100000000 + Math.random() * 900000000);
+  // console.log(reference)
+  // const getName = () => {
+  //   console.log("nameData.bankname.transferData.beneficiaryBankCode", bankData.bank);
+  //   return bankData.bank.find(
+  //     (element) =>
+  //       element.routingKey == nameData.bankname.transferData.beneficiaryBankCode
+  //   ).name;
+  // };
+  const reference = 're3z6182'
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    reqData({
+      nameEnquiryReference: nameData.bankname.transferData.nameEnquiryReference,
+      debitAccountNumber: nameData.bankname.transferData.debitAccountNumber,
+      beneficiaryAccountNumber: nameData.bankname.transferData.beneficiaryAccountNumber,
+      beneficiaryBankCode: nameData.bankname.transferData.beneficiaryBankCode,
+      beneficiaryAccountName: nameData.bankname.transferData.beneficiaryAccountName,
+      narration: nameData.bankname.transferData.narration,
+      amount:nameData.bankname.transferData.amount,
+      saveBeneficiary: nameData.bankname.transferData.saveBeneficiary,
+      saveBeneficiaryForUs:nameData.bankname.transferData.saveBeneficiaryForUs,
+      pin:combinedpin,
+      paymentReference:reference
+    });
+    console.log(deposit)
+    // setDepositState({...depositstate, ...{deposit}, ...{combinedpin:pin }})
+    // console.log(combinedpin)
+    // console.log(depositstate)
+    depositData(deposit);
+
+  }
   return (
     <div className="previewotherbank">
       <Navbar />
@@ -77,12 +131,12 @@ const PreviewOtherBank = ({
                 <div className="preview-1 preview-upper">
                   <div className="preview-left">
                     <p className="receipt-head">Bank Name</p>
-                    <p className="receipt-body">{getName()}</p>
+                    {/* <p className="receipt-body">{getName()}</p> */}
                   </div>
                   <div className="preview-right">
                     <p className="receipt-head">Amount</p>
                     <p className="receipt-body">
-                      N {nameData.bankname.transferData.amount}
+                      N{nameData.bankname.transferData.amount}
                     </p>
                   </div>
                 </div>
@@ -117,7 +171,6 @@ const PreviewOtherBank = ({
                     <input
                       type="text"
                       maxlength="1"
-                      value={pin}
                       onChange={onChangepin1}
                       ref={atmpin}
                       autoFocus
@@ -129,7 +182,6 @@ const PreviewOtherBank = ({
                     <input
                       type="text"
                       maxlength="1"
-                      value={pin1}
                       onChange={onChangepin2}
                       ref={atmpin1}
                     ></input>
@@ -140,7 +192,6 @@ const PreviewOtherBank = ({
                     <input
                       type="text"
                       maxlength="1"
-                      value={pin2}
                       onChange={onChangepin3}
                       ref={atmpin2}
                     ></input>
@@ -151,8 +202,7 @@ const PreviewOtherBank = ({
                     <input
                       type="text"
                       maxlength="1"
-                      value={pin3}
-                      onChange={onChangepin4}
+                      onBlur={onChangepin4}
                       ref={atmpin3}
                     ></input>
                   </div>
@@ -160,7 +210,8 @@ const PreviewOtherBank = ({
               </div>
               <div className="form-submit-right saving-submit withdraw-submit">
                 <button
-                  onClick={() => setOpenModal(true)}
+                  // onClick={() => setOpenModal(true)}
+                  onClick={handleSubmit}
                   type="submit"
                   value="Continue"
                   className="submit-2"
@@ -199,6 +250,7 @@ const mapStoreToProps = (state) => {
   return {
     bankData: state.bankname,
     nameData: state,
+    deposit: state.bankname.transferData
   };
 };
 
@@ -211,6 +263,9 @@ const mapDispatchToProps = (dispatch) => {
     reqData: (depositState) => {
       dispatch(reqData(depositState));
     },
+    depositData: (depositState)=>{
+      dispatch(depositData(depositState))
+  }
   };
 };
 
